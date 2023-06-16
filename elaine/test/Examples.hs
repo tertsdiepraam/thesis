5e5683bfb997ef2f8efb68989304e7c76ddf3189
Terts Diepraam <terts.diepraam@gmail.com>
2023-06-16 14:10:39 +0200

    replace code examples with tested versions

--- elaine/test/Examples.hs
+++ elaine/test/Examples.hs
@@ -18,13 +18,15 @@ import Test.Hspec
 expected :: String -> Value
 expected "state.elaine" = Int 6
 expected "id.elaine" = Int 5
-expected "abort.elaine" = Int 5
+expected "abort.elaine" = Unit
+expected "safe_division.elaine" = Int 5
 expected "implicit.elaine" = String "The answer is: 23"
 expected "basics.elaine" = Int 8
 expected "structured_logging.elaine" = String "main: msg1\nmain:foo: msg2\nmain:bar: msg3\n"
 expected "square_is_even.elaine" = String "The square of 4 is even"
 expected "local_reader.elaine" = Int 4
 expected "logic.elaine" = String "False, True, True\nFalse, True, False\n"
+expected "val.elaine" = Int 32
 expected _ = error "Example does not have an expected value"
 
 testAllExamples :: SpecWith ()
